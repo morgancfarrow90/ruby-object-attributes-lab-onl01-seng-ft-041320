08f6861d9a93b849bdcd84f994988b8b8f254e36
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-21 13:35:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,17 @@
 class Dog 
-  
-  def initialize(name)
-    @name = name 
-  end
+  def name= (name)
+    @name = name
+  end  
   
   def name 
     @name 
   end
   
-  def name=(new_name)
-    @name = new_name
+  def breed= (breed)
+    @breed = breed
   end
   
+  def breed
+    @breed
+  end
 end 
